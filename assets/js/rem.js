81dcf5dee49328b442adebe5bd9521643fbcdcd7
oliver <lixiao470@qq.com>
2017-12-20 22:32:58 +0800

    Update rem.js

--- assets/js/rem.js
+++ assets/js/rem.js
@@ -2,13 +2,13 @@
  * 移动端适配(640px设计稿)
  */
 function rem () {
-  let clientWidth = document.documentElement.clientWidth
+  var clientWidth = document.documentElement.clientWidth
   if (clientWidth > 640) clientWidth = 640
   document.documentElement.style.fontSize = clientWidth / 6.4 + 'px'
   document.documentElement.style.width = '100%'
 }
 
-let resize = 'orientationchange' in window ? 'orientationchange' : 'resize'
+var resize = 'orientationchange' in window ? 'orientationchange' : 'resize'
 
 document.addEventListener(resize, rem, false)
 window.addEventListener('DOMContentLoaded', rem, false)
